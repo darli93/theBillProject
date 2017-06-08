1bc8ec0a7b8217157e9aa2cab0c79d8f4a788e99
Lin <hu.lha86@gmail.com>
2017-06-08 18:00:03 +0200

    Aggiunto get/set footer

--- src/main/java/parser/it/fides/fatture/parsing/datacontainers/Footer.java
+++ src/main/java/parser/it/fides/fatture/parsing/datacontainers/Footer.java
@@ -2,6 +2,22 @@ package it.fides.fatture.parsing.datacontainers;
 
 public class Footer {
 	
+	private String methodOfPayment = new String();
+	private String bankId = new String();
 	
+	public String getMethodOfPayment() {
+		return methodOfPayment;
+	}
+	public void setMethodOfPayment(String methodOfPayment) {
+		this.methodOfPayment = methodOfPayment;
+	}
+	public String getBankId() {
+		return bankId;
+	}
+	public void setBankId(String bankId) {
+		this.bankId = bankId;
+	}
+	
+
 	
 }
