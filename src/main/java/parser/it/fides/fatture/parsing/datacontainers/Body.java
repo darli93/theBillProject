25e0b6799f8f13e7bd2e284b5cccb02dab453618
Lin <hu.lha86@gmail.com>
2017-06-09 09:08:13 +0200

    Dati fattura get & set

--- src/main/java/parser/it/fides/fatture/parsing/datacontainers/Body.java
+++ src/main/java/parser/it/fides/fatture/parsing/datacontainers/Body.java
@@ -1,5 +1,34 @@
 package it.fides.fatture.parsing.datacontainers;
 
 public class Body {
+	private String billFirstId = new String();
+	private String billDateOfIssue = new String();
+	private String billDueDate = new String();
+	private String billSecondId = new String();
+	
+	public String getBillFirstId() {
+		return billFirstId;
+	}
+	public void setBillFirstId(String billFirstId) {
+		this.billFirstId = billFirstId;
+	}
+	public String getBillDateOfIssue() {
+		return billDateOfIssue;
+	}
+	public void setBillDateOfIssue(String billDateOfIssue) {
+		this.billDateOfIssue = billDateOfIssue;
+	}
+	public String getBillDueDate() {
+		return billDueDate;
+	}
+	public void setBillDueDate(String billDueDate) {
+		this.billDueDate = billDueDate;
+	}
+	public String getBillSecondId() {
+		return billSecondId;
+	}
+	public void setBillSecondId(String billSecondId) {
+		this.billSecondId = billSecondId;
+	}
 
 }
