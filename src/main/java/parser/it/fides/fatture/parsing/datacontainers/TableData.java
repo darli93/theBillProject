736b222f735c74188bae25dedb62bb2bb29e3183
darligedaj <darligedaj@mbp-di-darli.fidesdigital>
2017-06-10 03:30:32 +0200

    //

---
+++ src/main/java/parser/it/fides/fatture/parsing/datacontainers/TableData.java
@@ -0,0 +1,74 @@
+package it.fides.fatture.parsing.datacontainers;
+
+public class TableData {
+	
+	private int quantita;
+	private String descrizione = new String();
+	private float price;
+	private String sc = new String();
+	int iva = 22;
+	private double imponibile;
+	
+	public int getQuantita() {
+	
+		return quantita;
+	
+	}
+	
+	public void setQuantita(int quantita) {
+	
+		this.quantita = quantita;
+	
+	}
+	
+	public String getDescrizione() {
+	
+		return descrizione;
+	
+	}
+	
+	public void setDescrizione(String descrizione) {
+	
+		this.descrizione = descrizione;
+	
+	}
+	
+	public float getPrice() {
+	
+		return price;
+	
+	}
+	
+	public void setPrice(float price) {
+	
+		this.price = price;
+	
+	}
+	
+	public String getSc() {
+	
+		return sc;
+	
+	}
+
+	public void setSc(String sc) {
+	
+		this.sc = sc;
+	
+	}
+	
+	public double getImponibile() {
+	
+		return imponibile;
+	
+	}
+	
+	public void setImponibile(double imponibile) {
+	
+		this.imponibile = imponibile;
+	
+	}
+
+	
+
+}
