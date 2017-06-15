28eb5bd6f7ddeedc018d6a09aae84e82ca362a9e
darligedaj <darligedaj@mbp-di-darli.fidesdigital>
2017-06-15 17:10:12 +0200

    New Elements

--- src/main/java/parser/it/fides/fatture/parsing/reading/TextReaderTest.java
+++ src/main/java/parser/it/fides/fatture/parsing/reading/TextReaderTest.java
@@ -1,32 +1,3 @@
 package it.fides.fatture.parsing.reading;
 
-import static org.junit.Assert.*;
 
-import java.io.FileNotFoundException;
-
-import org.junit.Test;
-
-import it.fides.fatture.parsing.datacontainers.Header;
-import it.fides.fatture.parsing.datacontainers.TheBill;
-
-public class TextReaderTest {
-
-	@Test
-	public void testRead() throws FileNotFoundException {
-		
-		TheBill fattura = new TheBill();
-		
-		Header head = new Header();
-  
-		head.setFirstLine("SPETT.LE");
-        head.setSecondLine("ACCENTURE TECHNOLOGY SOLUTIONS SRL");
-        head.setThirdLine("VIA QUADRIO,17");
-        head.setForthLine("20154 - MILANO - MI");
-        head.setFifthLine("PIVA 03646450969");
-		fattura.setHead(head);
-
-		assertEquals(fattura.getHead().getSecondLine(), TheBill.readBill().getHead().getSecondLine());
-		
-	}
-
-}
--- src/main/java/reporting/it/fides/fatture/reporting/pdfreport/WordHeadFoot.java
+++ src/main/java/reporting/it/fides/fatture/reporting/pdfreport/WordHeadFoot.java
@@ -1,139 +1,140 @@
 package it.fides.fatture.reporting.pdfreport;
 
-import java.util.*;
+//import java.util.*;
 import java.io.*;
 import java.math.*;
 
-import org.apache.poi.hssf.util.HSSFColor;
+//import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.*;
-import org.apache.poi.xssf.usermodel.*;
+//import org.apache.poi.xssf.usermodel.*;
 import org.apache.poi.xwpf.model.*;
 import org.apache.poi.xwpf.usermodel.*;   
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 
 public class WordHeadFoot {
+	
+	private static XWPFDocument document = new XWPFDocument(); 
+	private static XWPFHeaderFooterPolicy headerFooterPolicy = document.createHeaderFooterPolicy();
 
 	public static void main(String[] args) throws InvalidFormatException, FileNotFoundException, IOException {
 		
-		writeFootandHead();
-	
+		//XWPFHeader header = WordHeader();
+		XWPFFooter footer = WordFooter();
+		
+		FileOutputStream out = new FileOutputStream(new File("CreateDoc.docx"));
+	    document.write(out);
+	    out.close();
+	    System.out.println("createdocument.docx written successully");
+	    document.close();
+		
 	}
 	
+	public static XWPFHeader WordHeader() throws InvalidFormatException, FileNotFoundException, IOException {
+		
+		XWPFHeader header = headerFooterPolicy.createHeader(XWPFHeaderFooterPolicy.DEFAULT);
+	    XWPFParagraph paragraphHeader = header.createParagraph();
+	    paragraphHeader.setAlignment(ParagraphAlignment.LEFT);
+	    XWPFRun runHeader = paragraphHeader.createRun();  
+	    String imgLogoFile= "src/images/logo_Fides.png";
+	    runHeader.addPicture(new FileInputStream(imgLogoFile), XWPFDocument.PICTURE_TYPE_PNG, "Fides Digital", Units.toEMU(102), Units.toEMU(42));
+		
+	    return header;
+	    
+	}
 	
-	public static void writeFootandHead() throws InvalidFormatException, FileNotFoundException, IOException {
-		      //Blank Document
-		      XWPFDocument document= new XWPFDocument(); 
-		      
-		      // create header-footer
-		      XWPFHeaderFooterPolicy headerFooterPolicy = document.createHeaderFooterPolicy();
-//		      XWPFHeaderFooterPolicy headerFooterPolicy = document.getHeaderFooterPolicy();
-//		      if (headerFooterPolicy == null) headerFooterPolicy = document.createHeaderFooterPolicy();
-
-		      // create header start
-		      XWPFHeader header = headerFooterPolicy.createHeader(XWPFHeaderFooterPolicy.DEFAULT);
-
-		      XWPFParagraph paragraphHeader = header.createParagraph();
-		      paragraphHeader.setAlignment(ParagraphAlignment.LEFT);
-
-		      XWPFRun runHeader = paragraphHeader.createRun();  
-		      String imgLogoFile= "src/images/logo_Fides.png";
-		      runHeader.addPicture(new FileInputStream(imgLogoFile), XWPFDocument.PICTURE_TYPE_PNG, imgLogoFile, Units.toEMU(102), Units.toEMU(42));
-		      
-		      //create footer start
-		      XWPFFooter footer = headerFooterPolicy.createFooter(XWPFHeaderFooterPolicy.DEFAULT);
-		      
-		      XWPFParagraph paragraphFooter = footer.createParagraph();
-		      paragraphFooter.setAlignment(ParagraphAlignment.LEFT);
-		      
-		      //create table
-		      XWPFTable tableFooter = footer.createTable(1, 3);
-		      //set cell width
-		      CTTblWidth width = tableFooter.getCTTbl().addNewTblPr().addNewTblW();
-		      width.setType(STTblWidth.DXA);
-		      width.setW(BigInteger.valueOf(10000));
-		      //bordless table
-		      tableFooter.getCTTbl().getTblPr().unsetTblBorders();
-		      
-		      String[] prima = {"Fides Consulting S.r.l", "Via Motta Casa dei Miri, 21", "Gragnano (Na) - 80054", "C.F. e P.IVA 05131731217", "Cap. Sociale 90.000,00", "Rea Napoli 735203"};
-		      String[] seconda  = {"Sede Amministrativa", "Centro Direzionale Is. E7" , "80143 - Napoli (Italy)", "Phone +39 081 195 02 985 ", "Fax +39 081 195 02 948 ", "www.fides.it | info@fides.it"};
-		      String[] terza = {"Unità  Locale", "Viale Monza, 1", "20125 - Milano", "Phone +39 02 49 76 39 67", "Fax +39 02 49 76 39 85"};
-		      
-		      //create first row
-		      XWPFTableRow tableFooterRowOne = tableFooter.getRow(0);
-		      XWPFParagraph firstCellHeader = tableFooterRowOne.getCell(0).addParagraph();
-		      firstCellHeader.setSpacingAfter(0);
-		      XWPFRun firstCellParHeader = firstCellHeader.createRun();
-		      XWPFParagraph firstCellBody = tableFooterRowOne.getCell(0).addParagraph();
-		      XWPFRun firstCellParBody = firstCellBody.createRun();
-		      
-		      firstCellParHeader.setColor("1874CD");
-	    	  firstCellParHeader.setText(prima[0]);
-	    	  firstCellParHeader.setFontFamily("Open Sans");
-	    	  firstCellParHeader.setFontSize(9);
-	    	  firstCellParBody.setFontFamily("Open Sans");
-	    	  firstCellParBody.setFontSize(8);
+	public static XWPFFooter WordFooter() throws InvalidFormatException, FileNotFoundException, IOException {
+		
+		XWPFFooter footer = headerFooterPolicy.createFooter(XWPFHeaderFooterPolicy.DEFAULT);
+		XWPFParagraph paragraphFooter = footer.createParagraph();
+	    paragraphFooter.setAlignment(ParagraphAlignment.LEFT);
+	    
+	    XWPFTable tableFooter = footer.createTable(1, 3);
+	      //set cell width
+	      CTTblWidth width = tableFooter.getCTTbl().addNewTblPr().addNewTblW();
+	      width.setType(STTblWidth.DXA);
+	      width.setW(BigInteger.valueOf(10000));
+	      //bordless table
+	      tableFooter.getCTTbl().getTblPr().unsetTblBorders();
+	      
+	      String[] prima = {"Fides Consulting S.r.l", "Via Motta Casa dei Miri, 21", "Gragnano (Na) - 80054", "C.F. e P.IVA 05131731217", "Cap. Sociale 90.000,00", "Rea Napoli 735203"};
+	      String[] seconda  = {"Sede Amministrativa", "Centro Direzionale Is. E7" , "80143 - Napoli (Italy)", "Phone +39 081 195 02 985 ", "Fax +39 081 195 02 948 ", "www.fides.it | info@fides.it"};
+	      String[] terza = {"Unitï¿½ Locale", "Viale Monza, 1", "20125 - Milano", "Phone +39 02 49 76 39 67", "Fax +39 02 49 76 39 85"};
+		
+	      XWPFTableRow tableFooterRowOne = tableFooter.getRow(0);
+	      XWPFParagraph firstCellHeader = tableFooterRowOne.getCell(0).addParagraph();
+	      firstCellHeader.setSpacingAfter(0);
+	      XWPFRun firstCellParHeader = firstCellHeader.createRun();
+	      XWPFParagraph firstCellBody = tableFooterRowOne.getCell(0).addParagraph();
+	      XWPFRun firstCellParBody = firstCellBody.createRun();
+	      
+	      firstCellParHeader.setColor("1874CD");
+    	  firstCellParHeader.setText(prima[0]);
+    	  firstCellParHeader.setFontFamily("Open Sans");
+    	  firstCellParHeader.setFontSize(9);
+    	  firstCellParBody.setFontFamily("Open Sans");
+    	  firstCellParBody.setFontSize(8);
+    	  
+	      for(int i = 1; i < prima.length; i++) {
 	    	  
-		      for(int i = 1; i < prima.length; i++) {		    	  
-		    	  firstCellParBody.setColor("000000");
-		    	  firstCellParBody.setText(prima[i]);
-		    	  firstCellParBody.addBreak();  
-		      }
-		      
-		      XWPFParagraph SecondCellHeader = tableFooterRowOne.getCell(1).addParagraph();
-		      SecondCellHeader.setSpacingAfter(0);
-		      XWPFRun SecondCellParHeader = SecondCellHeader.createRun();
-		      XWPFParagraph SecondCellBody = tableFooterRowOne.getCell(1).addParagraph();
-		      XWPFRun SecondCellParBody = SecondCellBody.createRun();
-		      
-		      SecondCellParHeader.setColor("1874CD");
-		      SecondCellParHeader.setText(seconda[0]);
-		      SecondCellParHeader.setFontFamily("Open Sans");
-		      SecondCellParHeader.setFontSize(9);
-	    	  SecondCellParBody.setColor("000000");
-	    	  SecondCellParBody.setFontFamily("Open Sans");
-	    	  SecondCellParBody.setFontSize(8);
+	    	  firstCellParBody.setColor("000000");
+	    	  firstCellParBody.setText(prima[i]);
+	    	  firstCellParBody.addBreak();  
+	    
+	      }
+	      
+	      XWPFParagraph SecondCellHeader = tableFooterRowOne.getCell(1).addParagraph();
+	      SecondCellHeader.setSpacingAfter(0);
+	      XWPFRun SecondCellParHeader = SecondCellHeader.createRun();
+	      XWPFParagraph SecondCellBody = tableFooterRowOne.getCell(1).addParagraph();
+	      XWPFRun SecondCellParBody = SecondCellBody.createRun();
+	      
+	      SecondCellParHeader.setColor("1874CD");
+	      SecondCellParHeader.setText(seconda[0]);
+	      SecondCellParHeader.setFontFamily("Open Sans");
+	      SecondCellParHeader.setFontSize(9);
+    	  SecondCellParBody.setColor("000000");
+    	  SecondCellParBody.setFontFamily("Open Sans");
+    	  SecondCellParBody.setFontSize(8);
+    	  
+	      for(int i = 1; i < seconda.length; i++) {
 	    	  
-		      for(int i = 1; i < seconda.length; i++) {
-		    	  SecondCellParBody.setText(seconda[i]); 
-		    	  SecondCellParBody.addBreak();
-		      }
-		      
-		      XWPFParagraph ThirdCellHeader = tableFooterRowOne.getCell(2).addParagraph();
-		      ThirdCellHeader.setSpacingAfter(0);
-		      XWPFRun ThirdCellParHeader = ThirdCellHeader.createRun();
-		      XWPFParagraph ThirdCellBody = tableFooterRowOne.getCell(2).addParagraph();
-		      XWPFRun ThirdCellParBody = ThirdCellBody.createRun();
-		      
-		      ThirdCellParHeader.setColor("1874CD");
-		      ThirdCellParHeader.setText(terza[0]);
-		      ThirdCellParHeader.setFontFamily("Arial");
-		      ThirdCellParHeader.setFontSize(9);
-		      
-	    	  ThirdCellParBody.setColor("000000");
-	    	  ThirdCellParBody.setFontFamily("Open Sans");
-		      ThirdCellParBody.setFontSize(8);
-		      for(int i = 1; i < terza.length; i++) {
-		    	  ThirdCellParBody.setText(terza[i]);
-		    	  ThirdCellParBody.addBreak();
-		      }
-		      
-		      //add image
-		      XWPFParagraph addNewTableCellPar = tableFooter.getRow(0).addNewTableCell().addParagraph();
-		      XWPFRun addNewCellRun = addNewTableCellPar.createRun();
-		      String imgAccredia= "src/images/accredia.jpg";
-		      XWPFPicture accrediaCell = addNewCellRun.addPicture(new FileInputStream(imgAccredia), XWPFDocument.PICTURE_TYPE_JPEG, imgAccredia, Units.toEMU(61), Units.toEMU(78));
-		      String imgKiwa= "src/images/kiwa.jpg";
-		      XWPFPicture kiwaCell = addNewCellRun.addPicture(new FileInputStream(imgKiwa), XWPFDocument.PICTURE_TYPE_JPEG, imgAccredia, Units.toEMU(61), Units.toEMU(96));
+	    	  SecondCellParBody.setText(seconda[i]); 
+	    	  SecondCellParBody.addBreak();
+	     
+	      }
+	      
+	      XWPFParagraph ThirdCellHeader = tableFooterRowOne.getCell(2).addParagraph();
+	      ThirdCellHeader.setSpacingAfter(0);
+	      XWPFRun ThirdCellParHeader = ThirdCellHeader.createRun();
+	      XWPFParagraph ThirdCellBody = tableFooterRowOne.getCell(2).addParagraph();
+	      XWPFRun ThirdCellParBody = ThirdCellBody.createRun();
+	      
+	      ThirdCellParHeader.setColor("1874CD");
+	      ThirdCellParHeader.setText(terza[0]);
+	      ThirdCellParHeader.setFontFamily("Arial");
+	      ThirdCellParHeader.setFontSize(9);
+	      
+    	  ThirdCellParBody.setColor("000000");
+    	  ThirdCellParBody.setFontFamily("Open Sans");
+	      ThirdCellParBody.setFontSize(8);
+	      
+	      for(int i = 1; i < terza.length; i++) {
+	    	  
+	    	  ThirdCellParBody.setText(terza[i]);
+	    	  ThirdCellParBody.addBreak();
+	     
+	      }
+	      
+	      //add image
+	      XWPFParagraph addNewTableCellPar = tableFooter.getRow(0).addNewTableCell().addParagraph();
+	      XWPFRun addNewCellRun = addNewTableCellPar.createRun();
 
-		      //Write the Document in file system
-		      FileOutputStream out = new FileOutputStream(new File("CreateDoc.docx"));
-		      document.write(out);
-		      out.close();
-		      System.out.println("createdocument.docx written successully");
-		      document.close();
-		      //document.close();
-		}
-	
+	      addNewCellRun.addPicture(new FileInputStream("src/images/accredia.jpg"), XWPFDocument.PICTURE_TYPE_JPEG, "Accredia", Units.toEMU(61), Units.toEMU(78));
+	      addNewCellRun.addPicture(new FileInputStream("src/images/kiwa.jpg"), XWPFDocument.PICTURE_TYPE_JPEG, "kiwa", Units.toEMU(61), Units.toEMU(96));
+	      
+		return footer;
+		
+	}
+
 }
-
